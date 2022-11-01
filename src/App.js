55a90ed737539ed8e35a085f71a30b6a12f24260
pandu926 <63030609+pandu926@users.noreply.github.com>
2022-11-02 01:41:15 +0700

    add login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./page/LandingPage";
+import Login from "./page/Login";
+import Register from "./page/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
