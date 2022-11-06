5a6e06af0ce8b78b327a24e2963796ffed256ff4
Shafar Yulianto Nugroho <45268908+katashafar@users.noreply.github.com>
2022-11-06 21:49:41 +0700

    finishing landing page, add animation depedency

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LandingPage from "./page/LandingPage";
-
+import Hero from "./pages/Hero";
+import Gallery from "./pages/Gallery";
+import Service from "./pages/Service";
+import Faq from "./pages/Faq";
+import Footer from "./pages/Footer";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Hero /> &nbsp;
+      <Gallery />
+      <Service />
+      <Faq />
+      <Footer />
+    </div>
   );
 }
 
