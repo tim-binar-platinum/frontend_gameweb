7e5119d2c3c0c50d4645afe6fa124336dc02fc2c
Shafar Yulianto Nugroho <45268908+katashafar@users.noreply.github.com>
2022-11-08 01:29:41 +0700

    delete unecessary files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import Hero from "./pages/Hero";
-import Gallery from "./pages/Gallery";
-import Service from "./pages/Service";
-import Faq from "./pages/Faq";
-import Footer from "./pages/Footer";
 function App() {
   return (
-    <div>
-      <Hero /> &nbsp;
-      <Gallery />
-      <Service />
-      <Faq />
-      <Footer />
+    <div> Hello world
     </div>
   );
 }
