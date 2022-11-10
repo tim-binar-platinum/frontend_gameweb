b8694434fbfbc911d4c94d6cef333cc0564bec47
Ilfan Alwirudin <muhammadilfan7@gmail.com>
2022-11-10 19:06:13 +0700

    change alt description on game-list

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Gallery1 from "../assets/image/suit.jpg";
-import Gallery2 from "../assets/image/preorder.jpg";
-import Gallery3 from "../assets/image/preorder.jpg";
-import Gallery4 from "../assets/image/preorder.jpg";
-import Gallery5 from "../assets/image/preorder.jpg";
-import Gallery6 from "../assets/image/preorder.jpg";
+import Gallery2 from "../assets/image/coming-soon.jpg";
+import Gallery3 from "../assets/image/coming-soon.jpg";
+import Gallery4 from "../assets/image/coming-soon.jpg";
+import Gallery5 from "../assets/image/coming-soon.jpg";
+import Gallery6 from "../assets/image/coming-soon.jpg";
 const Gallery = () => {
   return (
     <div className="gallery min-vh-100 d-flex align-items-center" id="gallery">
@@ -17,28 +17,28 @@ const Gallery = () => {
         >
           <Col>
             <Link to="/game-suit">
-              <img src={Gallery1} alt="unsplah.com" className="w-100" />
+              <img src={Gallery1} alt="coming-soon" className="w-100" />
               <p className="text-center fw-bold">Rock Paper Scisor</p>
             </Link>
           </Col>
           <Col>
-            <img src={Gallery2} alt="unsplah.com" className="w-100" />
+            <img src={Gallery2} alt="coming-soon" className="w-100" />
             <p className="text-center fw-bold">Coming Soon</p>
           </Col>
           <Col>
-            <img src={Gallery4} alt="unsplah.com" className="w-100" />
+            <img src={Gallery4} alt="coming-soon" className="w-100" />
             <p className="text-center fw-bold">Coming Soon</p>
           </Col>
           <Col>
-            <img src={Gallery5} alt="unsplah.com" className="w-100" />
+            <img src={Gallery5} alt="coming-soon" className="w-100" />
             <p className="text-center fw-bold">Coming Soon</p>
           </Col>
           <Col>
-            <img src={Gallery6} alt="unsplah.com" className="w-100" />
+            <img src={Gallery6} alt="coming-soon" className="w-100" />
             <p className="text-center fw-bold">Coming Soon</p>
           </Col>
           <Col>
-            <img src={Gallery3} alt="unsplah.com" className="w-100" />
+            <img src={Gallery3} alt="coming-soon" className="w-100" />
             <p className="text-center fw-bold">Coming Soon</p>
           </Col>
         </Row>
