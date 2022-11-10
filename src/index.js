f99e173d76b3324be10f6e80a5a2e5eef141149f
Shafar Yulianto Nugroho <45268908+katashafar@users.noreply.github.com>
2022-11-10 17:25:36 +0700

    add admin dashboard and profile page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
