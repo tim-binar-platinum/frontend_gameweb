644449afcbdf230f5c7202be0eea5516df6eb82c
ilfan <muhammadilfan7@gmail.com>
2022-11-11 11:20:23 +0000

    deploy main to server

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
       email: email,
       password: password,
     };
-    fetch("https://pencarikhuntul.lol/auth/login", {
+    fetch("http://103.181.143.76:4000/login", {
       method: "POST",
       headers: {
         Accept: "application/json",
